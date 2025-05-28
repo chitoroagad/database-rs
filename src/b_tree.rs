312ed40bc7f366cb351270e0cc832364fe5aaff3
chitoroagad <darius.chitoroaga@pm.me>
2025-05-28 23:58:10 +0100

    made debug message

--- src/b_tree.rs
+++ src/b_tree.rs
@@ -1,18 +1,30 @@
-const BTREE_PAGE_SIZE: u64 = 4096;
+pub const BTREE_PAGE_SIZE: usize = 4096;
 const BTREE_MAX_KEY_SIZE: u64 = 1000;
 const BTREE_MAX_VAL_SIZE: u64 = 3000;
 const BTREE_MAX_NODE_SIZE: u64 = 4 + 1 * 8 + 1 * 2 + 4 + BTREE_MAX_KEY_SIZE + BTREE_MAX_VAL_SIZE;
 
-mod node {
-    use std::ops::Range;
+pub mod node {
+    use std::{fmt::{self, Debug, Formatter}, ops::Range};
 
     use super::BTREE_PAGE_SIZE;
 
-    struct BNode<'a> {
-        data: &'a mut [u8; BTREE_PAGE_SIZE as usize],
+    pub struct BNode<'a> {
+        data: &'a mut [u8; BTREE_PAGE_SIZE],
+    }
+
+    impl<'a> Debug for BNode<'a> {
+        fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+            f.debug_struct("BNode")
+            .field("data", &&self.data[..]) // `&&` converts `&mut [u8; N]` to `&&[u8]`
+            .finish()
+        }
     }
 
     impl<'a> BNode<'a> {
+        pub fn new(data: &'a mut [u8; BTREE_PAGE_SIZE]) -> Self {
+            BNode { data }
+        }
+
         fn btype(&self) -> u16 {
             u16::from_le_bytes([self.data[0], self.data[1]])
         }
@@ -21,7 +33,7 @@ mod node {
             u16::from_le_bytes([self.data[2], self.data[3]])
         }
 
-        fn set_header(self, btype: u16, nkeys: u16) {
+        pub fn set_header(self, btype: u16, nkeys: u16) {
             let btype = btype.to_le_bytes();
             let nkeys = nkeys.to_le_bytes();
 
@@ -29,7 +41,7 @@ mod node {
             self.data[2..4].copy_from_slice(&nkeys);
         }
 
-        fn get_ptr(self, idx: u16) -> u64 {
+        pub fn get_ptr(self, idx: u16) -> u64 {
             let pos: usize = 4 + 8 * (idx as usize); // header is 4 bytes
             let bytes = [
                 self.data[pos],
@@ -44,14 +56,14 @@ mod node {
             u64::from_le_bytes(bytes)
         }
 
-        fn set_ptr(self, idx: u16, val: u64) {
+        pub fn set_ptr(self, idx: u16, val: u64) {
             assert!(&self.nkeys().ge(&idx));
             let pos = 4 + 8 * (idx as usize);
             let bytes = val.to_le_bytes();
             self.data[pos..pos + 8].copy_from_slice(&bytes);
         }
 
-        fn get_offset(&self, idx: u16) -> u16 {
+        pub fn get_offset(&self, idx: u16) -> u16 {
             if idx == 0 {
                 return 0;
             };
@@ -60,13 +72,13 @@ mod node {
             u16::from_le_bytes([self.data[pos], self.data[pos + 1]])
         }
 
-        fn kv_pos(&self, idx: u16) -> u16 {
+        pub fn kv_pos(&self, idx: u16) -> u16 {
             assert!(idx < self.nkeys());
 
             4 + 8 * self.nkeys() + 2 * self.nkeys() + self.get_offset(idx)
         }
 
-        fn get_key(self, idx: u16) -> &'a [u8] {
+        pub fn get_key(self, idx: u16) -> &'a [u8] {
             assert!(idx < self.nkeys());
 
             let pos = self.kv_pos(idx) as usize;
@@ -94,6 +106,6 @@ mod tests {
 
     #[test]
     fn max_node_size_fits_in_page() {
-        assert!(BTREE_MAX_NODE_SIZE <= BTREE_PAGE_SIZE);
+        assert!(BTREE_MAX_NODE_SIZE <= BTREE_PAGE_SIZE as u64);
     }
 }
